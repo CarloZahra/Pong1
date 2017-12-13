2a562f392dc78497477909a7f167d3264cd69f61
Carlo Zahra <carlo.zahra.b31268@mcast.edu.mt>
2017-12-13 05:31:33 +0100

    Level2
    Placeholder assets removed. Level 2 is almost complete. Remaining task on level 2:Carry over score from level 1

--- SHINPong/Assets/ballController.cs
+++ SHINPong/Assets/ballController.cs
@@ -2,28 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ballController : MonoBehaviour {
     // Use this for initialization
     GameObject p1Goal,p2Goal;
     public int p1Score,p2Score;
     Text p1Text, p2Text;
+    scoreKeeper ScoreKeeper;
     void Start () {
         GetComponent<Rigidbody2D>().velocity = new Vector2(-10f,0);
         p1Goal = GameObject.Find("P1Goal");
         p2Goal = GameObject.Find("P2Goal");
         p1Text= GameObject.Find("P1 Score").GetComponent<Text>();
         p2Text = GameObject.Find("P2 Score").GetComponent<Text>();
-        p1Score = 0;
-        p2Score = 0;
-        
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         float randomX = Random.Range(-2f, 2f);
-        GetComponent<Rigidbody2D>().velocity += new Vector2(randomX, randomX);
+        GetComponent<Rigidbody2D>().velocity += new Vector2(0, randomX);
         if (collision.gameObject == p1Goal)
         {
             transform.position = new Vector3(0,0, 0);
@@ -42,6 +41,14 @@ public class ballController : MonoBehaviour {
     void Update () {
         p1Text.text = p1Score.ToString();
         p2Text.text = p2Score.ToString();
+        if (p1Score == 5 || p2Score == 5)
+        {
+            SceneManager.LoadScene("level2");
+            ScoreKeeper.Truep1Score+=p1Score;
+            ScoreKeeper.Truep2Score+=p2Score;
+            
+            
+        }    
     }
    
 
---
+++ SHINPong/Assets/scoreKeeper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreKeeper : MonoBehaviour {
+
+    public int Truep1Score, Truep2Score = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
