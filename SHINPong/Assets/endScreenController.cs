94aa64fca251adb2db22b8896b554f96e9665579
Carlo Zahra <carlo.zahra.b31268@mcast.edu.mt>
2017-12-15 21:19:15 +0100

    Programmed End Screen
    Shows who the winner is and the final scores

--- SHINPong/Assets/endScreenController.cs
+++ SHINPong/Assets/endScreenController.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class endScreenController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Text p1FinalText,p2FinalText,wonResult;
+    scoreKeeper ScoreKeeper;
+    // Use this for initialization
+    void Start () {
+        p1FinalText = GameObject.Find("P1 Final Score").GetComponent<Text>();
+        p2FinalText = GameObject.Find("P2 Final Score").GetComponent<Text>();
+        wonResult = GameObject.Find("Won").GetComponent<Text>();
+        ScoreKeeper = GameObject.Find("ScoreKeeper").GetComponent<scoreKeeper>();
+        p1FinalText.text += ScoreKeeper.Truep1Score.ToString();
+        p2FinalText.text += ScoreKeeper.Truep2Score.ToString();
+        if (ScoreKeeper.Truep1Score > ScoreKeeper.Truep2Score)
+        {
+            wonResult.text = "Player 1 is the winner";
+        }
+        else if (ScoreKeeper.Truep2Score > ScoreKeeper.Truep1Score)
+        {
+            wonResult.text = "Player 2 is the winner";
+        }
+        else if (ScoreKeeper.Truep1Score > ScoreKeeper.Truep2Score)
+        {
+            wonResult.text = "Draw";
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+    }
 }
